954949e9daf1ddad1426b1b9a2f1680b69f811c5
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-11-09 16:58:32 -0300

    Config-Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Config from './pages/Config';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/game" component={ Game } />
+        <Route exact path="/config" component={ Config } />
       </Switch>
     );
   }
