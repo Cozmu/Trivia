f13a51c977d68708c760fb9dd973a96a2cf68b2b
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-11-09 15:30:46 -0300

    store-main

---
+++ src/redux/store/store.js
@@ -0,0 +1,14 @@
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
+
+if (window.Cypress) {
+  window.store = store;
+}
+
+export default store;
