14033a0e56a6e94305aa1dbfcb189f40ccd6610e
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-11-09 15:31:42 -0300

    main-index-provider-and-store-done

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './redux/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store>
+  <Provider store={ store }>
     <BrowserRouter>
       <App />
     </BrowserRouter>
