ef4426046810115f650378d41f5394d5bee69209
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-11-09 16:54:40 -0300

    game-route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 class App extends React.Component {
   render() {
