283c089059dc28c4cf5a3210e21efa63800e06f9
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-11-09 17:04:20 -0300

    req-3-done

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,19 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 class Game extends React.Component {
   render() {
     return (
-      <div>Jogo</div>
+      <main>
+        <h1>Jogo</h1>
+      </main>
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 export default Game;
