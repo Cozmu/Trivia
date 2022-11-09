e118489fce86ad9bdcbb5158df554ae56a28549f
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-11-09 15:33:39 -0300

    main-store-done

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  primeiroReducer,
+});
+
+export default rootReducer;
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,7 @@
 import { composeWithDevTools } from '@redux-devtools/extension';
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
 
 const store = createStore(
   rootReducer,
