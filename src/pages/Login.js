52523f23dda9f3a33fdf63b31e2bc94619901c29
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-11-10 16:07:13 -0300

    dispatch-email-and-name

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Login extends React.Component {
   state = {
@@ -24,9 +25,12 @@ class Login extends React.Component {
     fetch('https://opentdb.com/api_token.php?command=request')
       .then((response) => response.json())
       .then((data) => {
+        const { email, name } = this.state;
+        const { dispatch } = this.props;
         localStorage.setItem('token', data.token);
         const { history } = this.props;
         history.push('/game');
+        dispatch(saveNameAndEmail(email, name));
       });
   };
 
@@ -60,7 +64,6 @@ class Login extends React.Component {
           disabled={ !disabled }
           data-testid="btn-play"
           type="button"
-
           onClick={ this.fetchToken }
 
         >
@@ -91,6 +94,12 @@ Login.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func,
   }).isRequired,
+  dispatch: PropTypes.string.isRequired,
 };
 
-export default Login;
+// const mapStateToProps = (store) => ({
+//   gravatarEmail: store.player.gravatarEmail,
+//   name: store.player.name,
+// });
+
+export default connect(mapStateToProps)(Login);
