f9bf8a95d3570e6239507fcea45439bef344c834
Cozmu <www.jorgereis@gmail.com>
2022-11-10 22:25:35 -0300

    Add componente cronometro

---
+++ src/components/Cronometro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Cronometro extends React.Component {
+  render() {
+    return (
+      <div>
+        {}
+      </div>
+    );
+  }
+}
+
+export default Cronometro;
