082b1b41a868c46e9de577a9333ca5e8f2aed5d0
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-11-11 15:29:23 -0300

    Feedback-page-creating

---
+++ src/pages/Feedback.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <h1 data-testid="settings-title">Feedback</h1>
+    );
+  }
+}
+export default Feedback;
