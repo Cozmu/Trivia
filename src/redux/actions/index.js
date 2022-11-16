7bbf1ac55cfe4ee4583e4d19a89ad290fc0a011b
Breno Almeida <brenoedalmeida@gmail.com>
2022-11-16 14:54:40 -0300

    main-group-4-req-19 done

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -48,3 +48,8 @@ export const DEFAULT_CORRECT = 'DEFAULT_CORRECT';
 export const defaultCorrect = () => ({
   type: DEFAULT_CORRECT,
 });
+
+export const RESET_SCORE = 'RESET_SCORE';
+export const resetScore = () => ({
+  type: RESET_SCORE,
+});
